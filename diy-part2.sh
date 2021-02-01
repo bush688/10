5626a1760e57e447d59960dc7aa5a36f6788cc5f
bush688 <32064695+bush688@users.noreply.github.com>
2021-02-01 23:27:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 #
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
@@ -6,9 +6,9 @@
 # See /LICENSE for more information.
 #
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
+ File name: diy-part2.sh
+ Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+ Modify default IP
+sed -i 's/192.168.2.1/192.168.50.5/g' package/base-files/files/bin/config_generate
